186aaf8750791d6c7113e140ee60529e56b8f339
PANICBUTT0N <82723611+PANICBUTT0N@users.noreply.github.com>
2024-10-22 17:41:23 -0400

    Complete base functionality

--- src/__init__.py
+++ src/__init__.py
@@ -2,20 +2,23 @@ from aqt import mw
 from aqt.browser import Browser
 from aqt.qt import *
 from anki.hooks import addHook
-from aqt.utils import (
-    skip_if_selection_is_empty,
-    showInfo,
-    qconnect
-)
+from aqt.utils import showInfo, qconnect
 import re
 
 FIELDS_DICT = {}
-for note_type in mw.col.models.all():
-    all_fields_dict = note_type['flds']
-    fields_list = [field_name['name'] for field_name in all_fields_dict]
-    FIELDS_DICT.update({note_type['name']: fields_list})
-FIELDS = list(set(field for fields_list in FIELDS_DICT.values() for field in fields_list))
-FIELDS.insert(0, 'All fields')
+models = []
+
+
+def retrieve_note_types():
+    global models, FIELDS_DICT
+    if mw and mw.col:
+        models = mw.col.models.all()
+        for note_type in models:
+            all_fields_dict = note_type['flds']
+            fields_list = [field_name['name'] for field_name in all_fields_dict]
+            FIELDS_DICT[note_type['name']] = fields_list
+
+addHook("profileLoaded", retrieve_note_types)
 
 
 def regex(search_field, pattern):
@@ -37,22 +40,28 @@ def regex(search_field, pattern):
 
 
 def search_in_browser(results):
+    search_query = 'nid:' + ','.join(map(str, results))
+    showInfo(search_query)
     browser = Browser(mw)
-    browser.show()
-
-    search_query = 'nid:' + ','.join(results)
-    browser.searchEdit.setText(search_query)
-
-    browser.onSearch()
+    browser.form.searchEdit.lineEdit().setText(search_query)
+    browser.onSearchActivated()
 
 
 def regex_search_window():
+    if not models:
+        showInfo("Note types have not been loaded yet!")
+        return
+
     dialog = QDialog(mw)
     dialog.setWindowTitle('Select a field:')
     layout = QVBoxLayout()
 
+    all_fields = list(set(field for fields_list in FIELDS_DICT.values() for field in fields_list))
+    all_fields.sort()
+    all_fields = ['All fields'] + all_fields
+
     field_dropdown = QComboBox()
-    field_dropdown.addItems(FIELDS)
+    field_dropdown.addItems(all_fields)
     layout.addWidget(field_dropdown)
 
     text_label = QLabel('Enter your regular expression:')
@@ -65,22 +74,28 @@ def regex_search_window():
     layout.addWidget(ok_button)
 
     dialog.setLayout(layout)
-
     dialog.exec()
 
 
 def on_ok(dialog, field_dropdown, regex_input):
     selected_field = field_dropdown.currentText()
-    regex_input = regex_input.text()
-    results = regex(selected_field, regex_input)
+    regex_input_text = regex_input.text()
+    results = regex(selected_field, regex_input_text)
     search_in_browser(results)
     dialog.accept()
 
 
-def setup_menu():
-    action = QAction('Regex Search', mw)
-    action.triggered.connect(regex_search_window)
-    mw.browser.form.menu_Notes.addAction(action)
+def setup_menu(browser: Browser) -> None:
+    menu_notes = browser.form.menu_Notes
+    menu_notes.addSeparator()
+    regex_search = QAction('Regex Search', menu_notes)
+    regex_search.setObjectName('regex_search')
+    qconnect(regex_search.triggered, lambda: regex_search_window())
+    menu_notes.addAction(regex_search)
 
 
-setup_menu()
+def main():
+    addHook('browser.setupMenus', setup_menu)
+
+
+main()
